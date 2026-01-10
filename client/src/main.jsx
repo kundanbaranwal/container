874d88e1086f519ee9f2bb820d52a8818a777d3e
Kundan baranwal <baranwalk624@gmail.com>
2026-01-10 12:40:06 +0530

    third

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,6 +6,8 @@ import axios from "axios";
 
 if (import.meta.env.PROD) {
   axios.defaults.baseURL = import.meta.env.VITE_API_URL;
+} else {
+  axios.defaults.baseURL = "/api";
 }
 
 ReactDOM.createRoot(document.getElementById("root")).render(
